7daa0726b9133cbe9c1dd67e352e59c8bd2f8ba6
Nguyễn Văn Dĩ Khang <dizzkawq@gmail.com>
2025-11-25 14:53:58 +0700

    [PHP][CLIENT][PRODUCTS] hiển thị ra tất cả sản phẩm ở trang index

--- Controllers/Client/HomeController.php
+++ Controllers/Client/HomeController.php
@@ -1,18 +1,21 @@
 <?php
 require_once "Models/CategoryModel.php";
+require_once "Models/ProductModel.php";
 class HomeController
 {
     private $categoryModel;
+    private $productModel;
 
     public function __construct()
     {
         $this->categoryModel = new CategoryModel();
+        $this->productModel = new ProductModel();
     }
 
     public function index()
     {
         $categories = $this->categoryModel->getAllCategories();
+        $products = $this->productModel->getAllProducts();
         require_once "Views/client/index.php";
-
     }
 }
--- Models/ProductModel.php
+++ Models/ProductModel.php
@@ -1,23 +1,18 @@
 <?php
-
 require_once "Database.php";
-
 class ProductModel
 {
     private $connection;
-
+    private $table = "products";
     public function __construct()
     {
         $database = new Database();
         $this->connection = $database->connect();
     }
-
     public function getAllProducts()
     {
-        $stmt = $this->connection->prepare("SELECT * FROM products");
+        $stmt = $this->connection->prepare("SELECT * FROM " . $this->table . " WHERE status = 1");
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
-}  
-
-?>
+}
