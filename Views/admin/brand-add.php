c5b9190998088a4dbc968f46fc42feb12696a1af
Ong Tuấn Nghĩa <ongtuannghia2006@gmail.com>
2025-11-26 07:40:42 +0700

    [HTML][ADMIN] - Thương hiệu

---
+++ Views/admin/brand-add.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once "header.php";
+require_once "layouts/sidebar.php";
+
+?>
+
+        <div class="relative flex flex-col flex-1 overflow-x-hidden overflow-y-auto">
+            <div @click="sidebarToggle = false" :class="sidebarToggle ? 'block lg:hidden' : 'hidden'" class="fixed w-full h-screen z-9 bg-gray-900/50"></div>
+            <?php require_once "layouts/header.php"; ?>
+            <?php require_once "modules/brand/add.php"; ?>
+        </div>
+
+<?php
+
+require_once "footer.php";
+
+?>
