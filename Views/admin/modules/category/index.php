ce5e430b0c4ed38d69ad9d2288100c8fd3e35728
Nhat3635 <minhnhat4645@gmail.com>
2025-11-28 08:47:05 +0700

    [PHP][ADMIN] - Update - sửa đường dẫn khi sửa ảnh

--- Views/admin/modules/category/index.php
+++ Views/admin/modules/category/index.php
@@ -132,7 +132,7 @@
                                                 <div class="flex items-center">
                                                     <div class="flex items-center gap-3">
                                                         <div class="w-10 h-10 overflow-hidden rounded-full">
-                                                            <img src="Assets/category/thumbnail/<?= $category['thumbnail'] ?>"
+                                                            <img src="Assets/client/img/<?= $category['image'] ?>"
                                                                 alt="thumbnail" />
                                                         </div>
                                                         <div>
